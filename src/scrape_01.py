598fcce1d890030cbace4aa82ef0c4f1b321a4f7
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-02-23 20:04:41 +0100

    a light start - ver. 0.0.2

--- src/scrape_01.py
+++ src/scrape_01.py
@@ -1,18 +1,30 @@
-import urllib2
 
-url = '7116808'
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+from bs4 import BeautifulSoup
 
-response = urllib2.urlopen(url)
-webContent = response.read()
+with open("sites.csv", "r") as fils:
+    lst_sites = fils.readlines()
+site1 = [tok.strip().replace('"','') for tok in lst_sites[0].split(',')]
+print(site1)
+mmsi = 7116808
+str_url = "{}{}{}{}".format(site1[0], site1[1], str(mmsi), site1[2])
+print(str_url)
 
-f = open('7116808.html', 'w')
-f.write(webContent)
-f.close
 
+http = urllib3.PoolManager()
+r = http.request('GET', str_url)
+print(r.status)
+#200
+soup = BeautifulSoup(r.data, 'html.parser')
 
----
+mydivs = soup.findAll("div", {"class": "row"})
+keyval = soup.find("div", "key-value-table")
+print(str(type(keyval)), keyval)
+for desn in keyval.descendants:
+    print('\n', desn)
+#imo = keyval.findnext('div', "key")
+#print(imo)
 
-# query the website and return the html to the variable 'page'
-page = urllib.request.urlopen(urlpage)
-# parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(page, 'html.parser')
+with open('7116808.html', 'w') as filo:
+    filo.write(str(soup))
