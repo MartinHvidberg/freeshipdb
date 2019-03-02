e5938b8f85b2176d67c560b8f1e19aaf414dd12f
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-03-02 09:44:00 +0100

    scraping 2 sites, only missing Next function - ver. 0.2.3

--- src/scrape_02.py
+++ src/scrape_02.py
@@ -4,24 +4,27 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from bs4 import BeautifulSoup as bs
 
-str_url = r"www.shipnumber.com/"
-http = urllib3.PoolManager()
+LST_KEYT = ['IMO number', 'MMSI', 'Vessel Name', 'Call Sign', 'Vessel Type',
+            'flag', 'Gross Tonnage', 'Size', 'Year', 'update time']
+HEADER = "IMO number,MMSI,Vessel Name,Call Sign,Vessel Type," \
+         "flag,Gross Tonnage,Size,Year,update time"
+STR_FILO = "shipnumbers.csv"
 
-def scrape_shipnumber(str_url, dic_ships):
+def scrape_shipnumber_page(str_url, dic_ships):
     print("GET: {}".format(str_url))
 
     r = http.request('GET', str_url)
     soup = bs(r.data, 'html.parser')
-    tabl = soup.findAll('table')[0]
 
+    # Harvest table
+    tabl = soup.findAll('table')[0]
     if tabl:
         num_row = 0
         for row in tabl.findAll('tr'):
             if num_row == 0:
-                lst_header = [cel.text.strip() for cel in row.findAll('td')]
+                lst_header = [cel.text.replace(',',';').strip() for cel in row.findAll('td')]
             else:
-                lst_values = [cel.text.strip() for cel in row.findAll('td')]
-                #print(lst_values)
+                lst_values = [cel.text.replace(',',';').strip() for cel in row.findAll('td')]
                 dic_tmp = dict()
                 for n in range(len(lst_header)):
                     dic_tmp[lst_header[n]] = lst_values[n]
@@ -32,23 +35,30 @@ def scrape_shipnumber(str_url, dic_ships):
             num_row += 1
     else:
         print("No table found on page: {}".format())
-    return "", dic_ships
 
-dic_ships = dict()
-while str_url:
-    str_url, dic_ships = scrape_shipnumber(str_url, dic_ships)
+    # Harves Next
+    str_next = ""
 
-with open("shipnumbers.csv", 'a') as filo:
-    filo.write("IMO number,MMSI,Vessel Name,Call Sign,Vessel Type,flag,Gross Tonnage,Size,Year,update time" + '\n')
-    for keys in dic_ships.keys():
-        #print("ship: {} > {}".format(keys, dic_ships[keys]))
-        str_ship = str()
-        for keyt in ['IMO number','MMSI','Vessel Name','Call Sign','Vessel Type','flag','Gross Tonnage','Size','Year','update time']:
-            #print(dic_ships[keys][keyt])
-            str_ship = str_ship + dic_ships[keys][keyt] + ', '
-        str_ship = str_ship.strip().strip(',')
-        print("|{}|".format(str_ship))
-        filo.write(str_ship+'\n')
+    return str_next, dic_ships
+
+if __name__ == '__main__':
+
+    str_url = r"www.shipnumber.com/"
+    http = urllib3.PoolManager()
+    # Open out file and write header
+    with open(STR_FILO, 'w') as filo:
+        filo.write(HEADER+'\n')
+    # Scrape webpages to file
+    while str_url:
+        str_url, dic_ships = scrape_shipnumber_page(str_url, dict())
+        with open(STR_FILO, 'a') as filo:
+            for keys in dic_ships.keys():
+                str_ship = str()
+                for keyt in LST_KEYT:
+                    str_ship = str_ship + dic_ships[keys][keyt] + ', '
+                str_ship = str_ship.strip().strip(',')
+                print("|{}|".format(str_ship))
+                filo.write(str_ship+'\n')
 
 
 #
