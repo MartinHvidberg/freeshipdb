eeb70006e2998ac11aa6fd3d2320f55c7ebfcddc
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-03-06 16:34:30 +0100

    scrape_01 ran through, en like 10 days

---
+++ src/first_digit_stats.sql
@@ -0,0 +1,20 @@
+﻿select imo/1000000, count(*)
+  from ais.shpnms_scrp
+  where imo > 999999 
+    and imo < 10000000
+  group by imo/1000000
+  order by imo/1000000
+  --limit 999
+;
+
+-- smaller
+select count(*)
+  from ais.shpnms_scrp
+  where imo <= 999999
+;
+
+-- større
+select count(*)
+  from ais.shpnms_scrp
+  where imo >= 10000000
+;
---
+++ src/imo_duplicates.sql
@@ -0,0 +1,13 @@
+﻿with mimo as (
+select imo
+  from ais.shpnms_scrp
+  group by imo
+  having count(*) > 4
+  order by count(*) desc
+)
+select *
+  from ais.shpnms_scrp
+  where imo in (select imo from mimo)
+    and imo != 0
+  order by imo
+;
