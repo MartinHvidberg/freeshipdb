71a97c33d5b9db617442ee2f94be51d9dd7b629c
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-03-19 21:00:38 +0100

    ships SEQ20190319EOD

--- src/ship_update.py
+++ src/ship_update.py
@@ -29,6 +29,16 @@ except (Exception, psycopg2.Error) as error:
 
 # Hardcoded constants
 IMO = '8917613'
+LST_FLD = ['imo', 'mmsi', 'callsign', 'ship_name', 'ship_type', 'flag',
+           'ship_length', 'ship_width',
+           'ship_weight_gt', 'ship_weight_dw', 'ship_draught',
+           'ship_status', 'home_port', 'build_year', 'build_place',
+           'ship_owner', 'ship_operator', 'classification_society',
+           'former_names', 'info_source', 'info_update']
+
+
+def same_dic(dic_a, dic_b):
+    return False
 
 
 def get_fieldnames_from_table(table, schema='ais'):
@@ -39,36 +49,94 @@ def get_fieldnames_from_table(table, schema='ais'):
         cur.execute(str_sql)
         lst_fld = cur.fetchall()
     lst_ret = [tok[0] for tok in lst_fld]
-    print("fldret", lst_ret)
     return lst_ret
 
 
 def get_data_from_table(key, val, table, lst_fld, schema='ais'):
+    """ Get the relevant row(s) from a table, and then REMOVE them from the table """
     str_sql = "SELECT * FROM {}.{} WHERE {} = {};".format(schema, table, key, val)
     with con.cursor() as cur:
         cur.execute(str_sql)
         lst_val = cur.fetchall()
-    print(lst_val)
-    dic_ret = dict()
-    for n in range(len(lst_fld)):
-        fldn = lst_fld[n][0]
-        valn = lst_val[0][n]
-        dic_ret[fldn] = valn
-    return dic_ret
+    #print("raw sql res N={}: {}".format(len(lst_val), lst_val))
+    lst_ret = list()
+    for raw in lst_val:
+        dic_ret = dict()
+        for n in range(len(lst_fld)):
+            fldn = lst_fld[n]
+            valn = lst_val[0][n]
+            dic_ret[fldn] = valn
+        lst_ret.append(dic_ret)
+
+    # The REMOVE part
+    # TBI
+    return lst_ret
 
 
-def update_local_collect(imo=''):
+def insert_dic_in_coll(dic_cand):
+    imlpement this now ...
+
+def check_swap_length_width(dic_in):
+    if all ([para in dic_in.keys() for para in ['ship_length', 'ship_width']]):
+        if dic_in['ship_width'] > dic_in['ship_length']:  # if it's wider than long
+            dic_in['ship_width'], dic_in['ship_length'] = dic_in['ship_length'], dic_in['ship_width']
+    return dic_in
+
+
+def homogene_names(lst_in, source_id):
+    """ Take a dic, based on the local names from a scrape source,
+    and convert them to common collection names"""
+
+    def substitute(dic_in, str_a, str_b):
+        if str_a in dic_in.keys():
+            dic_in[str_b] = dic_in[str_a]
+            del dic_in[str_a]
+        return dic_in
+
+    lst_ret = list()
+    for dic_in in lst_in:
+        if source_id == 'vestra_scrp':  # Vessel Tracker
+            # Expect: ['shipname', 'mmsi', 'imo', 'imovalid', 'callsign', 'aistype', 'shiplength', 'shipwidth', 'flag']
+            dic_in = substitute(dic_in, 'aistype', 'ship_type')
+            dic_in = substitute(dic_in, 'shiplength', 'ship_length')
+            dic_in = substitute(dic_in, 'shipwidth', 'ship_width')
+            dic_in['info_source'] = 'vesseltracker.com'
+        elif source_id == 'shpnms_scrp':  # Ship Names
+            # Expect: ['ida', 'imo', 'mmsi', 'ship_name', 'callsign', 'ship_type', 'flag', 'ship_weight_gt', 'ship_size_a', 'ship_size_b', 'xxx_year', 'xxx_update_time']
+            dic_in = substitute(dic_in, 'ship_size_a', 'ship_length')
+            dic_in = substitute(dic_in, 'ship_size_b', 'ship_width')
+            dic_in = check_swap_length_width(dic_in)
+            dic_in = substitute(dic_in, 'xxx_year', 'build_year')
+            dic_in = substitute(dic_in, 'xxx_update_time', 'info_update')
+            dic_in['info_source'] = 'shipnumber.com'
+        lst_ret.append(dic_in)
+
+    return lst_ret
+
+
+def update_local_collect(imo):
     """ For the given IMO
     Walk all known _scrp tables
     transfer all (new) records to the collect table. """
     # get the _scrp data
-    lst_flds_vestra = get_fieldnames_from_table('vestra_scrp')
-    lst_vstra = get_data_from_table('imo', imo, 'vestra_scrp', lst_flds_vestra)
-    print(str(type(lst_vstra)), len(lst_vstra), lst_vstra)
-    lst_flds_shpnms = get_fieldnames_from_table('shpnms_scrp')
-    lst_shpnm = get_data_from_table('imo', imo, 'shpnms_scrp', lst_flds_shpnms)
-    print(str(type(lst_shpnm)), len(lst_shpnm), lst_shpnm)
+    for scrp_source in ['vestra_scrp', 'shpnms_scrp']:
+        lst_flds_source = get_fieldnames_from_table(scrp_source)
+        #print("fldret {} : {}".format(scrp_source, lst_flds_source))
+        lst_scrp = get_data_from_table('imo', imo, scrp_source, lst_flds_source)
+        lst_scrp = homogene_names(lst_scrp, scrp_source)
+        print("scpret {} {} {}".format(scrp_source, len(lst_scrp), lst_scrp))
+
     # get existing collect data
+    lst_flds_source = get_fieldnames_from_table('ship_collect')
+    lst_coll = get_data_from_table('imo', imo, 'ship_collect', lst_flds_source)
+    print("colret {} {} {}".format(scrp_source, len(lst_coll), lst_coll))
+
+    # Evaluate scrapes for potential adding to collect
+    for dic_cand in lst_scrp:
+        if not any ([same_dic(dic_cand, dic_coll) for dic_coll in lst_coll]):
+            insert_dic_in_coll(dic_cand)
+
+
     lst_colle = 0
 
 def update_local_mui(imo=''):
@@ -81,7 +149,6 @@ if __name__ == '__main__':
 
     # 1 Look ship up in local data
     update_local_collect(IMO)  # Collect data from all available scrp tables
-    update_local_mui(IMO)  # Update Most Updated Info, based on all collected info
 
     # 2 Look ship up on the internet
 
@@ -89,5 +156,6 @@ if __name__ == '__main__':
     # 3 Compare, and maybe update, local data
 
 
-    # 4 show up-to-date ship data
+    # 4 Present up-to-date ship data
+    update_local_mui(IMO)  # Update Most Updated Info, based on all collected info
     ship_show.main(IMO)
