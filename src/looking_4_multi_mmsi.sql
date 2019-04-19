af2d9994a544795c842019274c97179692585462
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-04-19 12:05:07 +0200

    ships including SQLs

---
+++ src/looking_4_multi_mmsi.sql
@@ -0,0 +1,31 @@
+﻿select imo, mmsi, count(*)
+  from ais.ship_collect
+  group by imo, mmsi
+  order by count(*) desc
+;
+
+select *
+  from ais.ship_collect
+  where imo = 0
+    and mmsi = 0
+;
+
+with psbt as (
+select imo, mmsi, count(*)
+  from ais.ship_collect
+  where mmsi != 0
+    and mmsi is not null
+  group by imo, mmsi
+  order by count(*) desc
+)
+select mmsi, count(*)
+  from psbt
+  group by mmsi
+  having count(*) > 1
+  order by count(*) desc
+;
+
+select *
+  from ais.ship_collect
+  where mmsi = 666000001
+;
