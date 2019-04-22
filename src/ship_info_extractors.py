0df3b361f855aa43cb053d00a05dcbf9060463b6
MartinHvidberg <MartinHvidberg@Tortoise.helium.lomanet>
2019-04-22 13:24:17 +0200

    ships 0.1.1 LUOTI collected 2 'extractors' in ship_info_extractors.py

--- src/ship_info_extractors.py
+++ src/ship_info_extractors.py
@@ -3,14 +3,18 @@
 each designed to scrape ship info from a specific web site.
 It would be nice if there were only one, but sites are uneven things ...
 
-Each function should receive a text-string with the full html of a web page, about one ship
+Each function should receive a 'bytes' data type, with the full html of a web page.
 and a dictionary (maybe empty) to put data into, and return.
-Each function returns the dictionary, with the relevant info added, semi-cleaned. """
+Each function returns the dictionary, with the relevant info added, semi-cleaned.
 
+The 'bytes' input is designed to come from a http.request('GET', str_url).data request,
+assuming http is a urllib3.PoolManager(). """
 
 from bs4 import BeautifulSoup as bs
+from bs4.element import Tag
 
-# VesselTracker
+
+# www.vesseltracker.com
 def esi_vt(rdata, dic_ret=None):
     if dic_ret == None:
         dic_ret = dict()
@@ -52,3 +56,43 @@ def esi_vt(rdata, dic_ret=None):
 
     # Return
     return dic_ret
+
+
+# www.marinetraffic.com
+def esi_marinetraffic_com(byt_html, dic_specs):
+    soup = bs(byt_html, 'html.parser')
+    #print(soup.prettify())
+
+    # Get top-box and 'Position Received'
+
+    def spans2dic(tag, dic_ret):
+        for child in tag.children:
+            if type(child) == Tag:
+                kv = list()
+                for grandchild in child.children:
+                    if type(grandchild) == Tag:
+                        gtt = grandchild.get_text()
+                        kv.append(gtt)
+                if len(kv) == 2:
+                    dic_ret[kv[0].strip().rstrip(':').strip().lower()] = kv[
+                        1].strip()
+        return dic_ret
+
+    for span in soup.findAll('span'):  # Targetet to scrape Top-box and
+        if 'AIS Vessel Type' in span.text:
+            dic_specs = spans2dic(span.parent.parent, dic_specs)
+        if 'Status:' in span.text:
+            dic_specs = spans2dic(span.parent.parent, dic_specs)
+
+    # Get 'General'
+
+    for div in soup.findAll("div", {"id": "details_wiki_accordion"}):
+        for hfour in div.findAll('h4'):
+            if hfour.get_text() == 'General':
+                for span in hfour.parent.parent.parent.findAll('span'):
+                    kv = [tok.strip() for tok in
+                          span.get_text().strip().split(':')]
+                    if len(kv) == 2:
+                        dic_specs[kv[0].strip().rstrip(':').strip().lower()] = kv[1].strip()
+
+    return dic_specs
--- src/ship_luoti.py
+++ src/ship_luoti.py
@@ -1,9 +1,11 @@
 
+import datetime
+
 import certifi
 import urllib3
-from bs4 import BeautifulSoup as bs
-from bs4.element import Tag
 import pprint
+
+import ship_info_extractors as sie
 
 
 """
@@ -16,6 +18,7 @@ Return the list of dic's
 # Hardcoded constants
 IMO = 7375765
 
+
 def scan_marinetraffic_com(id, mode='imo'):
     """ Scan https://www.marinetraffic.com/en/ais/details/ships/imo:xxxxxxx
     Compile a dic report, and return it... """
@@ -25,56 +28,31 @@ def scan_marinetraffic_com(id, mode='imo'):
     user_agent = {'user-agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0'}
     http = urllib3.PoolManager(10, headers=user_agent, cert_reqs='CERT_REQUIRED', ca_certs=certifi.where())
     r = http.request('GET', str_url)
-    soup = bs(r.data, 'html.parser')
-    #print(soup.prettify())
-
     dic_specs = dict()  # For collecting all specs about the ship
+    dic_ship = sie.esi_marinetraffic_com(r.data, dic_specs)
+    # Add meta data
+    dic_ship['info_source'] = 'www.marinetraffic.com'
+    dic_ship['info_update'] = datetime.datetime.now().isoformat()
+    return dic_ship
 
-    # Get top-box and 'Position Received'
-    
-    def spans2dic(tag, dic_ret):
-        for child in tag.children:
-            if type(child) == Tag:
-                kv = list()
-                for grandchild in child.children:
-                    if type(grandchild) == Tag:
-                        gtt = grandchild.get_text()
-                        kv.append(gtt)
-                if len(kv) == 2:
-                    dic_ret[kv[0].strip().rstrip(':').strip().lower()] = kv[1].strip()
-        return dic_ret
-
-    for span in soup.findAll('span'):  # Targetet to scrape Top-box and
-        if 'AIS Vessel Type' in span.text:
-            dic_specs = spans2dic(span.parent.parent, dic_specs)
-        if 'Status:' in span.text:
-            dic_specs = spans2dic(span.parent.parent, dic_specs)
-
-    # Get 'General'
-
-    for div in soup.findAll("div", {"id": "details_wiki_accordion"}):
-        for hfour in div.findAll('h4'):
-            if hfour.get_text() == 'General':
-                for span in hfour.parent.parent.parent.findAll('span'):
-                    kv = [tok.strip() for tok in span.get_text().strip().split(':')]
-                    if len(kv) == 2:
-                        dic_specs[kv[0].strip().rstrip(':').strip().lower()] = kv[1].strip()
-
-    pprint.pprint(dic_specs)
 
 def scan(imo, mode='imo'):
     lst_resi = list()
     # TBI: https://www.balticshipping.com/vessel/imo/8888630
     dic_resu = scan_marinetraffic_com(imo, 'imo')
+    lst_resi.append(dic_resu)
+    return lst_resi
 
 
 def main(imo):
     lst_ret = list()  # Initialise list to return
     print("Scanning inet for: {}".format(imo))
-    scan(imo, 'imo')
-    return lst_ret
+    lst_resi = scan(imo, 'imo')
+    return lst_resi
 
 if __name__ == '__main__':
 
     imo = IMO  # Go with hardcoded default
-    main(imo)
+    lst_res = main(imo)
+    for dic_resu in lst_res:
+        pprint.pprint(dic_resu)
